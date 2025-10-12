c3e41e46b1f7c921bd83f40d021ae499762fb8cb
AS-Gaspar <airtontunni@gmail.com>
2025-10-12 19:37:37 -0300

    .

---
+++ function.sh
@@ -0,0 +1,27 @@
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+[ -f "/usr/share/fzf/key-bindings.zsh" ] && source "/usr/share/fzf/key-bindings.zsh"
+[ -f "/usr/share/fzf/completion.zsh" ] && source "/usr/share/fzf/completion.zsh"
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+export PATH=$HOME/.local/bin:$HOME/.cargo/bin:$PATH
+export PATH="$HOME/.heroku-cli/heroku/bin:$PATH"
+eval "$(fasd --init auto)"
+
+alias lsd="exa --icons"
+alias bat="bat --style=auto"
+alias pgstop="sudo systemctl stop postgresql"
+alias pgstart="sudo systemctl start postgresql"
+
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+
+setopt appendhistory
+setopt sharehistory
+setopt extendedhistory
+setopt hist_ignore_dups
+
